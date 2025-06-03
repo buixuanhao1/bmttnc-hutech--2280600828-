e3406263d5c94ef1c04fab75a55b5b132355b927
hao bui <hbui40537@gmail.com>
2025-06-03 11:21:46 +0700

    buoi 4

--- lap_01/ex01/hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello, world!")
-print("My name is hao")
-print("Hutech University")
--- lap_01/ex02/ex02_05.py
+++
@@ -1,6 +0,0 @@
-soGL = float(input("Nhập số giờ làm mỗi tuần: "))
-luong = float(input("Nhập thù lao trên mỗi giờ làm tiêu chuẩn: "))
-gio_tieu_chuan = 44
-gio_vuot_chuan = max(0, soGL - gio_tieu_chuan)
-thuc_linh = gio_tieu_chuan * luong + gio_vuot_chuan * luong * 1.5
-print(f"Số tiền thực lĩnh cua rnhana viên : {thuc_linh}")
